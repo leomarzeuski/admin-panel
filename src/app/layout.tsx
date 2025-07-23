6ec4926ad394546b63fe67ff49e074436a922255
Higor Felipe Ribeiro <higorfeliperibeiro67@gmail.com>
2025-07-22 21:08:52 -0300

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Admin Panel - DealerSpace",
+  description: "Sistema administrativo DealerSpace",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster position="top-right" expand={true} richColors closeButton />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
