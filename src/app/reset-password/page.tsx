2b75e8c3acc0de1917f4b7daa0183532b0ffb2c6
HigorDevWeb <higorfeliperibeiro67@gmail.com>
2025-07-22 21:45:07 -0300

    fix:envio de email

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -72,7 +72,7 @@ function ResetPasswordPageInner() {
 
         setIsLoading(true);
         try {
-            const res = await fetch("https://api.itfolkstech.com/api/auth/reset-password", {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/auth/forgot-password`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
