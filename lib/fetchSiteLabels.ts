ff9c671b5e92593689850dce5603f73a79607333
Higor Felipe Ribeiro <higorfeliperibeiro67@gmail.com>
2025-07-22 21:05:23 -0300

    Delete lib directory

--- lib/fetchSiteLabels.ts
+++
@@ -1,6 +0,0 @@
-export async function fetchSiteLabels() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/site-ui-label?populate=*`);
-    if (!res.ok) throw new Error("Erro ao buscar labels do Strapi");
-    const json = await res.json();
-    return json.data; // ou .data.attributes, se quiser só o objeto de campos
-}
