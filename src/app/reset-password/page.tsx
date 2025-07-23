3c89db9e3729d1e14dc087a8d61db56ec487e5a4
HigorDevWeb <higorfeliperibeiro67@gmail.com>
2025-07-22 23:09:17 -0300

    Update API URL in reset password components to use STRAPI_URL environment variable

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -16,8 +16,7 @@ import {
 import { DealerLogo } from "../../components/DealerLogo";
 import { ModeToggle } from "@/components/ThemeToggle";
 import { Mail, ArrowLeft, CheckCircle, Send, Lock } from "lucide-react";
-const api_url =
-  process.env.NEXT_PUBLIC_API_URL || "https://api.itfolkstech.com"; // Ajuste a URL da API conforme necessário
+const api_url = process.env.NEXT_PUBLIC_STRAPI_URL || "https://api.itfolkstech.com";
 // Componente separado para ser usado no Suspense
 function ResetPasswordPageInner() {
   const searchParams = useSearchParams();
